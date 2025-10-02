1a78487d271378abad4806bb191308b5b8142c07
Euclides Medina Dagua <euclidesmeda10@gmail.com>
2025-10-02 09:21:45 -0500

    feat: conectar logica para cambio dinamico

--- ui/layout/Layout.py
+++ ui/layout/Layout.py
@@ -2,24 +2,36 @@ import flet as ft
 from ..components.header import Header
 from ..components.sidebar import Sidebar
 from ..components.show_content import ShowContent
+from core.file_manager import FileManager
 
 class Layout(ft.Container):
     def __init__(self, page:ft.Page = None):
         super().__init__()
         self.page = page
         self.expand = True
+        self.file_maneger = FileManager()
+
+        #components
+        self.header = Header(self.file_maneger, self.refresh)
+        self.sidebar = Sidebar()
+        self.show_content = ShowContent(self.file_maneger, self.refresh)
 
         self.content = ft.Column(
             spacing=0,
             expand=True,
             controls=[
-                Header(),
+                self.header,
                 ft.ResponsiveRow(
                     expand=True,
                     controls=[
-                        Sidebar(),
-                        ShowContent()
+                        self.sidebar,
+                        self.show_content
                     ]
                 )
             ]
         )
+
+    def refresh(self):
+        self.header.update_path()
+        self.show_content.refresh()
+        self.page.update()
