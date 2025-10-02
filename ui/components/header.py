6c2c4be7a3830e4c6ea7d7fb38b1d95471bb0e91
Euclides Medina Dagua <euclidesmeda10@gmail.com>
2025-10-02 11:40:24 -0500

    feat: agregar btn nuevo archivo/carpeta

--- ui/components/header.py
+++ ui/components/header.py
@@ -4,12 +4,10 @@ from core.file_manager import FileManager as fm
 class Header(ft.Container):
     def __init__(self, file_maneger:fm, refresh_callback):
         super().__init__()
-        # self.page = page
         self.file_manager = file_maneger
         self.refresh_callback = refresh_callback
 
-        self.content = ft.Row(
-            # spacing=0,
+        self.content_navigation = ft.Row(
             controls=[
                 ft.IconButton(
                     icon=ft.Icons.ARROW_BACK_SHARP,
@@ -23,16 +21,27 @@ class Header(ft.Container):
                     expand=True,
                     border = ft.border.all(0.5, "grey"),
                     padding=10,
+                    border_radius=5,
                     content=ft.Row(
                         expand=True,
                         controls= [
-                            ft.Text(f"Ruta: {self.file_manager.current_path}", weight="bold")
+                            ft.Text(f"Ruta: {self.file_manager.current_path}", weight="bold"),
                         ]
                     )
                 ),
+                ft.IconButton(
+                    icon=ft.Icons.NOTE_ADD,
+                    tooltip="Crear archivo"
+                ),
+                ft.IconButton(
+                    icon=ft.Icons.CREATE_NEW_FOLDER,
+                    tooltip="Crear carpeta"
+                )
                 
             ]
         )
+
+        self.content = self.content_navigation
 
     def go_back(self, e):
         if self.file_manager.go_back():
@@ -44,4 +53,4 @@ class Header(ft.Container):
 
 
     def update_path(self):
-        self.content.controls[2].content.controls[0].value = f"Ruta: {self.file_manager.current_path}"
+        self.content_navigation.controls[2].content.controls[0].value = f"Ruta: {self.file_manager.current_path}"
