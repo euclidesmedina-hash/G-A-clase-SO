fe6589a179973ecba5bb5c415c95eb2b33fbff19
Euclides Medina Dagua <euclidesmeda10@gmail.com>
2025-10-02 09:19:06 -0500

    feat: agregar logica cabio dinamico

--- ui/components/show_content.py
+++ ui/components/show_content.py
@@ -1,15 +1,41 @@
 import flet as ft
+import os
+import platform
+from core.file_manager import FileManager as fm
 
 class ShowContent(ft.Container):
-    def __init__(self, page:ft.Page = None):
+    def __init__(self, file_maneger:fm, refresh_callback):
         super().__init__()
-        self.page = page
+        # self.page = page
+        self.file_manager = file_maneger
+        self.refresh_callback = refresh_callback
         # self.border = ft.border.all(1, "red")
         self.col = self.col = {"xs":7, "sm":8, "md":9, "lg":10}
+        self.column = ft.ListView()
+        
+        self.content = self.column
 
-        self.content = ft.Column(
-            # expand=True,
-            controls=[
-                # ft.Text("Show Content")
-            ]
-        )
+    # Se ejecuta automáticamente cuando ya está en la page  
+    def did_mount(self):
+        self.refresh()
+
+    def refresh(self):
+        self.column.controls.clear()
+
+        for item in self.file_manager.list_dir():
+            path = os.path.join(self.file_manager.current_path, item)
+            icon = ft.Icons.FOLDER if os.path.isdir(path) else ft.Icons.DESCRIPTION
+            # is_dir = os.path.isdir(path)
+
+            self.column.controls.append(
+                ft.Container(
+                    content=ft.Row([ft.Icon(icon), ft.Text(item, expand=True)]),
+                    on_click= lambda e, f=item: self.on_click_item(f),
+                    ink=True
+                )
+            )
+        self.update()
+
+    def on_click_item(self, item):
+        if self.file_manager.navigate_to(item):
+            self.refresh_callback()
