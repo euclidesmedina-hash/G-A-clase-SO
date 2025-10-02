fb8d40af853f2c495262bdf6157f067f83b2671f
Euclides Medina Dagua <euclidesmeda10@gmail.com>
2025-10-02 09:14:01 -0500

    feat: agregar logica de obtner ruta dinamica

--- ui/components/header.py
+++ ui/components/header.py
@@ -1,17 +1,24 @@
 import flet as ft
+from core.file_manager import FileManager as fm
 
 class Header(ft.Container):
-    def __init__(self, page:ft.Page = None):
+    def __init__(self, file_maneger:fm, refresh_callback):
         super().__init__()
-        self.page = page
-        # self.height = 50
-        # self.border = ft.border.only(bottom=ft.border.BorderSide(1, "grey"))
+        # self.page = page
+        self.file_manager = file_maneger
+        self.refresh_callback = refresh_callback
 
         self.content = ft.Row(
             # spacing=0,
             controls=[
-                ft.IconButton(icon=ft.Icons.ARROW_BACK_SHARP),
-                ft.IconButton(icon=ft.Icons.ARROW_FORWARD_SHARP),
+                ft.IconButton(
+                    icon=ft.Icons.ARROW_BACK_SHARP,
+                    on_click=self.go_back
+                ),
+                ft.IconButton(
+                    icon=ft.Icons.ARROW_FORWARD_SHARP,
+                    on_click=self.go_forward
+                ),
                 ft.Container(
                     expand=True,
                     border = ft.border.all(0.5, "grey"),
@@ -19,10 +26,22 @@ class Header(ft.Container):
                     content=ft.Row(
                         expand=True,
                         controls= [
-                            ft.Text("Ruta: ", weight="bold")
+                            ft.Text(f"Ruta: {self.file_manager.current_path}", weight="bold")
                         ]
                     )
                 ),
                 
             ]
         )
+
+    def go_back(self, e):
+        if self.file_manager.go_back():
+            self.refresh_callback()
+
+    def go_forward(self, e):
+        if self.file_manager.go_forward():
+            self.refresh_callback()
+
+
+    def update_path(self):
+        self.content.controls[2].content.controls[0].value = f"Ruta: {self.file_manager.current_path}"
